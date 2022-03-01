da3843d8bd052791efce0af0269a29d3150d6d7e
mammu1100 <mammu.1100@gmail.com>
2022-03-01 09:11:14 +0530

    hello file

---
+++ hello.sh
@@ -0,0 +1,5 @@
+hai hello namasthey vanakkam aadhab
+here we go 
+we are going to create a new file creating the hello.sh
+we are adding this file to the youtube repositary in remote repositary
+thats it
